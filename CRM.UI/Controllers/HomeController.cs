f56aac524482920fd1d05065882eef568a3df40c
Pradeepkumar Mahor <Pradeepkumar.Mahor@sensiaglobal.com>
2024-10-10 12:09:08 +0530

    UI Setup

--- CRM.UI/Controllers/HomeController.cs
+++ CRM.UI/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace CRM.UI.Controllers
             return View();
         }
 
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
